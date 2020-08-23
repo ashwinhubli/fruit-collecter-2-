7f255ea5f51079c640428dd5563b55e101b2f9d5
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2020-08-23 19:01:17 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,43 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        this.SecondScore = 0;
+        this.allScore = score+secondScore;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score,
+            SecondScore:this.score,
+            allScore:this.allScore
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
